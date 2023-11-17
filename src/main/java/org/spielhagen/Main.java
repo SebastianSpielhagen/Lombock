77387ede0dd313930ba45680376a1fbe56538884
SebastianSpielhagen <ssp.java@icloud.com>
2023-11-17 12:56:09 +0100

    reduce code

--- src/main/java/org/spielhagen/Main.java
+++ src/main/java/org/spielhagen/Main.java
@@ -4,42 +4,24 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
         Student student1 = new Student();
-        student1.setId(1);
-        student1.setName("Sebastian");
-        student1.setNname("Sp.");
-        student1.setAddress("Bochum");
-        student1.setAge(50);
-        student1.setGrade(10);
+        student1.setId(1); student1.setName("Sebastian"); student1.setNname("Sp.");
+        student1.setAddress("Bochum"); student1.setAge(50); student1.setGrade(10);
 
         Student student2 = new Student();
-        student2.setId(2);
-        student2.setName("Siqi");
-        student2.setNname("Z.");
-        student2.setAddress("Gelsenkirchen");
-        student2.setAge(28);
-        student2.setGrade(9);
+        student2.setId(2); student2.setName("Siqi"); student2.setNname("Z.");
+        student2.setAddress("Gelsenkirchen"); student2.setAge(28); student2.setGrade(9);
 
         Student student3 = new Student();
-        student3.setId(2);
-        student3.setName("Ivan");
-        student3.setNname("C.");
-        student3.setAge(38);
-        student3.setAddress("Köln");
-        student3.setGrade(9);
+        student3.setId(2); student3.setName("Ivan"); student3.setNname("C.");
+        student3.setAge(38); student3.setAddress("Köln"); student3.setGrade(9);
 
-       Teacher teacher = new Teacher ();
-         teacher.setId(1);
-            teacher.setName("Coach Dominic");
-            teacher.setSubject("Java");
-
+        Teacher teacher = new Teacher();
+        teacher.setId(1); teacher.setName("Coach Dominic"); teacher.setSubject("Java");
 
         Course course = new Course();
-        course.setId(1);
-        course.setName("Java Kurs - ffm-java-23-2 - neue fische GmbH");
-        course.setTeacher(teacher);
-        course.setStudents(List.of(student1, student2, student3));
+        course.setId(1); course.setName("Java Kurs - ffm-java-23-2 - neue fische GmbH");
+        course.setTeacher(teacher); course.setStudents(List.of(student1, student2, student3));
 
         System.out.println("--------------------------");
         System.out.println("Name des Kurses: " + course.getName());
